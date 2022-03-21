3f3594307542c2ee9f0422ea63c990da75397100
Anderson de Oliveira Silva <anderson.oliveirasilva@gmail.com>
2022-03-21 10:24:39 -0300

    Ajustes de responsividade

--- game-mania/src/app/views/home-header/home-header.component.ts
+++ game-mania/src/app/views/home-header/home-header.component.ts
@@ -6,21 +6,46 @@ import * as $ from 'jquery';
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.css']
 })
+
 export class HomeHeaderComponent implements OnInit {
+
+  private menusSistema = [
+    "Ofertas",
+    "Jogos",
+    "Consoles",
+    "Acessórios",
+    "Eletrônicos",
+    "Login",
+    "Pedidos"
+  ]
+
+  public menus: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-
+    this.loadMenus(window.innerWidth);
     /* Faz o controle de mostrar ou não o menu de barras. */
     $("#barras").click(function() {
       $("#menu").toggleClass("menu-ativo")
     })
   }
 
+  loadMenus = (comprimento: number) => {
+    this.menus = [];
+    this.menusSistema.forEach(m => {
+      if (comprimento <= 769 ||
+        m.toLowerCase() != "login" &&
+        m.toLowerCase() != "pedidos")
+        this.menus.push(m);
+    });
+  }
+
   onResize = (event: any) => {
 
     let comprimento = event.target.innerWidth;
+
+    this.loadMenus(comprimento);
 
     if (comprimento > 769) {
         if (!$("#menu").hasClass("menu-ativo")) {
