b0b5524b1f0a0964f2431057fe73a450b19dead7
Anderson de Oliveira Silva <anderson.oliveirasilva@gmail.com>
2022-04-09 08:36:10 -0300

    ajustando estrutura igual da aula

--- senai-cursos/senaicursos/src/test/java/senaicursos/TesteCurso.java
+++ senai-cursos/senaicursos/src/test/java/senaicursos/TesteCurso.java
@@ -1,7 +1,6 @@
 package senaicursos;
 
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -16,24 +15,14 @@ public class TesteCurso {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chrome-driver\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-	}
-	
-	@Test
-	public void testeNavegador1() {
-		driver.get("https://sp.senai.br/");
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		driver.findElement(By.id("oqEstabuscando")).sendKeys("gestão");
-		driver.findElement(By.className("btnBuscaJavaScript")).click();
-	}
-
+	}	
 
 	@Test
-	public void testeNavegador2() {
+	public void testeNavegador() {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://informatica.sp.senai.br/");
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("gestão");
 		driver.findElement(By.id("Busca1_btnBusca")).click();
-	}
-	
+	}	
 }
